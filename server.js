19fbf1a6f1795b1af146b08b71e8b59c25bc421c
Caleb <caleblove@live.com>
2023-03-13 15:49:02 +1100

    uploading

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 const port = process.env.SESSION_SECTRET || 8080;
 const session = require("express-session");
+const MemoryStore = require('memorystore')(session)
 const setCurrentUser = require("./middlewares/set_current_User");
 
 const logger = require("./middlewares/logger");
 const methodOverride = require("method-override");
 const dishController = require("./controllers/dish_controller");
 const sessionController = require("./controllers/session_controller");
+
+app.use(session({
+  cookie: { maxAge: 86400000 },
+  store: new MemoryStore({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  }),
+  resave: false,
+  secret: 'keyboard cat'
+}))
 
 app.set("view engine", "ejs");
 
@@ -17,12 +27,11 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(methodOverride());
-
+app.use(methodOverride);
 
 app.use(
   session({
-    secret: "process.env.SESSION_SECTRET || keyboard cat",
+    secret: process.env.SESSION_SECTRET || 'mistyrose',
     resave: false,
     saveUninitialized: true,
   })
